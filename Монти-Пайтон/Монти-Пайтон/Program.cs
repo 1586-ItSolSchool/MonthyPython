fdc9c7d1ac8a37f7248dc60670e814091b07ed09
1arroiz <perat2003@gmail.com>
2018-11-11 13:01:00 +0300

    Начало

---
+++ Монти-Пайтон/Монти-Пайтон/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Монти_Пайтон
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
